1c201f6578da084826a02ff40be263297246dfb3
Jorge Leonardo <klichorf123@hotmail.com>
2022-10-27 18:34:04 -0500

    sprint 2

---
+++ pedidos_1/src/models/index.ts
@@ -0,0 +1,6 @@
+export * from './sucursal.model';
+export * from './prospecto.model';
+export * from './producto-servicio.model';
+export * from './usuario.model';
+export * from './mascota.model';
+export * from './plan.model';
